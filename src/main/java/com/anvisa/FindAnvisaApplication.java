4655b5a7c8532c3dbfc131d28b386e860f163d29
Alessandro Fred <fredalessandro@gmail.com>
2018-06-29 01:41:22 -0300

    ambiente

--- src/main/java/com/anvisa/FindAnvisaApplication.java
+++ src/main/java/com/anvisa/FindAnvisaApplication.java
@@ -4,6 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.autoconfigure.jackson.Jackson2ObjectMapperBuilderCustomizer;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
@@ -16,7 +18,7 @@ import com.fasterxml.jackson.databind.util.ISO8601DateFormat;
 @EnableScheduling
 @EntityScan(basePackages = { "com.anvisa.model" })
 @EnableJpaRepositories("com.anvisa.repository")
-public class FindAnvisaApplication {
+public class FindAnvisaApplication extends SpringBootServletInitializer {
 
 	@Bean
 	public ScheduledTasks scheduledTasks() {
@@ -34,6 +36,11 @@ public class FindAnvisaApplication {
 			}
 		};
 	}
+	
+	@Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(FindAnvisaApplication.class);
+    }
 
 	public static void main(String[] args) {
 		SpringApplication.run(FindAnvisaApplication.class, args);
