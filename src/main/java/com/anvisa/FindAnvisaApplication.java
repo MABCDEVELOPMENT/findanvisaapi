ee6d6e767cd417d6608389856c7ee33391e20c4e
Alessandro Fred <fredalessandro@gmail.com>
2018-07-07 19:32:16 -0300

    POM.XML

--- src/main/java/com/anvisa/FindAnvisaApplication.java
+++ src/main/java/com/anvisa/FindAnvisaApplication.java
@@ -42,7 +42,7 @@ public class FindAnvisaApplication extends SpringBootServletInitializer {
         return application.sources(FindAnvisaApplication.class);
     }
 
-	/*public static void main(String[] args) {
+	public static void main(String[] args) {
 		SpringApplication.run(FindAnvisaApplication.class, args);
-	}*/
+	}
 }
