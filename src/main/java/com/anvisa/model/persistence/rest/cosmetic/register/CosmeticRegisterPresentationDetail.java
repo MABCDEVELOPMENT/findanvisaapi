e664a2bf7154f19b49d360a0106af48e19290235
Alessandro Fred <fredalessandro@gmail.com>
2018-10-09 00:11:00 -0300

    PROCESSO DETALHE

--- src/main/java/com/anvisa/model/persistence/rest/cosmetic/register/CosmeticRegisterPresentationDetail.java
+++ src/main/java/com/anvisa/model/persistence/rest/cosmetic/register/CosmeticRegisterPresentationDetail.java
@@ -1,5 +1,33 @@
 package com.anvisa.model.persistence.rest.cosmetic.register;
 
-public class CosmeticRegisterPresentationDetail {
+import java.util.ArrayList;
 
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+import com.anvisa.model.persistence.BaseEntity;
+import com.anvisa.rest.detalhe.comestico.registrado.detalhe.apresentacao.RegistradoApresentacaoFabricantesNacionais;
+
+@Entity
+@Table(name = "cosmetic_register_presente_dateil")
+public class CosmeticRegisterPresentationDetail extends BaseEntity {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	
+	private String nomeProduto;  
+    private String processo;
+    private String apresentacao;
+    private String categoria;
+    private ArrayList<PresentationCountryManufacturer> fabricantesNacionais;
+    private String formaFisica;
+    private String tonalidade;
+    private String prazoValidade;
+    private ArrayList<String> conservacao;
+    private ArrayList<String> destinacao;
+    private ArrayList<String> restricao;
+	
 }
--- src/main/java/com/anvisa/model/persistence/rest/cosmetic/register/PresentationCountryManufacturer.java
+++ src/main/java/com/anvisa/model/persistence/rest/cosmetic/register/PresentationCountryManufacturer.java
@@ -1,6 +1,19 @@
 package com.anvisa.model.persistence.rest.cosmetic.register;
 
-public class PresentationCountryManufacturer {
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+import com.anvisa.model.persistence.BaseEntity;
+
+@Entity
+@Table(name = "cosmetic_country_manufacturer")
+public class PresentationCountryManufacturer extends BaseEntity {
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
 	
 	private String cnpj;
 	private String razaoSocial;
